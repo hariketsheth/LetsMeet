c4a1851a5a487368bbe9c5d46eb6d6d1e1d1a150
hariketsheth <hariket.sukeshkumar2020@gmail.com>
2021-11-18 00:29:51 +0530

    Added Firebase Config

---
+++ src/config.ts
@@ -0,0 +1,9 @@
+export default {
+  apiKey: "AIzaSyAlyCdEa3N8CpvmtO3OJUixfX1K4gWNUsM",
+  authDomain: "letsmeet-hariket.firebaseapp.com",
+  projectId: "letsmeet-hariket",
+  storageBucket: "letsmeet-hariket.appspot.com",
+  messagingSenderId: "229288545977",
+  appId: "1:229288545977:web:ec8a8796fd14b4199c1568",
+  measurementId: "G-P9762RQ9KP"
+};
